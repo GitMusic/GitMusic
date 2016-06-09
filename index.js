4cfa8ac25d2c7f5ea42bf394164cb7e456f8f8a3
MetaDark <kurt.bruneau@gmail.com>
2016-06-08 21:35:31 -0400

    Refactored streamFirstResult

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const fs = require('fs');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const path = require('path');
 
 const cache = require('./config.json').cache;
 const Player = require('./service/player');
 const youtube = require('./service/providers/youtube');
 
+function stream(id) {
+    let cacheFile = path.join(cache.dir, id);
+    let stats = cache.enabled ? fs.statSync(cacheFile) : null;
+    youtube.stream(id).then(stream => {
+        stream.on('response', res => {
+            let size = parseInt(res.headers['content-length'], 10);
+            console.log('Started downloading: ' + id + ' (' + size + ' bytes)');
+
+            // Show download progress in stdout
+            let total = 0;
+            stream.on('data', chunk => {
+                total += chunk.length;
+                let percent = total / size * 100;
+                console.log('Downloaded: ' + percent.toFixed(2) + '% of ' + id);
+            });
+        });
+    });
+}
+
 function streamFirstResult(term) {
     return youtube.search(term)
-        .then(videos => {
-            let first = videos[0];
-            return [youtube.stream(first.id), first.id];
-        })
-        .spread((stream, id) => {
-            // Show download progress in stdout
-            stream.on('response', res => {
-                let size, total = 0;
-                size = parseInt(res.headers['content-length'], 10);
-                console.log('Started downloading: ' + id + ' (' + size + ' bytes)');
-
-                stream.on('data', chunk => {
-                    total += chunk.length;
-                    let percent = total / size * 100;
-                    console.log('Downloaded: ' + percent.toFixed(2) + '% of ' + id);
-                });
-            });
-
-            // Cache stream
-            if (cache.enabled) {
-                let cachefile = path.join(cache.dir, id);
-                stream.pipe(fs.createWriteStream(cachefile));
-            }
-
-            return stream;
-        });
+        .then(videos => videos[0].id)
+        .then(stream);
 }
 
 let player = new Player();
