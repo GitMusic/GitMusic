f66e7028232cb990de36fd2110721af88841c8eb
MetaDark <kurt.bruneau@gmail.com>
2016-06-27 01:31:47 -0400

    Created simple CLI player

--- core/index.js
+++ core/index.js
@@ -1,11 +1,60 @@
-const AudioStream = require('./audio-stream');
+const readline = require('readline');
+const process = require('process');
+
 const Player = require('./player');
 const youtube = require('./providers/youtube');
 
-let player = new Player();
-player.play();
+const player = new Player();
+const commands = {
+    'l': (...words) => {
+        const query = words.join(' ');
+        console.log('Searching: ' + query);
+        youtube.search(query)
+            .then(videos => videos[0].id)
+            .then(youtube.source)
+            .then(source => {
+                console.log('Loading: ' + query);
+                player.load(source);
+            });
+    },
+    'p': (...words) => {
+        if (words.length === 0) {
+            if (player.playing) {
+                console.log('Pausing');
+                player.pause();
+            } else {
+                console.log('Playing');
+                player.play();
+            }
+        } else {
+            if (!player.playing) player.play();
+            commands.l.apply(null, words);
+        }
+    },
+    's': (seconds) => {
+        console.log('Seeking: ' + seconds);
+        player.seek(seconds);
+    },
+    'q': () => {
+        console.log('Exiting');
+        process.exit();
+    }
+};
 
-youtube.search(process.argv.slice(2).join(' '))
-    .then(videos => videos[0].id)
-    .then(youtube.source)
-    .then(source => player.stream(new AudioStream(source)));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+}).on('line', line => {
+    const [command, ...args] = line.split(' ');
+    const action = commands[command];
+    if (!action) {
+        console.error('Invalid command: ' + command);
+        return;
+    }
+
+    action.apply(null, args);
+});
+
+if (process.argv.length > 2) {
+    commands.l.apply(null, process.argv.slice(2));
+}
--- core/player.js
+++ core/player.js
@@ -1,39 +1,44 @@
 const Speaker = require('speaker');
+
+const AudioStream = require('./audio-stream');
 const config = require('../config.json').playback;
 
 class Player {
     constructor() {
         this._playing = false;
-        this._stream = null;
+        this._audio = null;
         this._speaker = new Speaker({
             sampleRate: config.sampleRate,
             channels: config.channels,
             bitDepth: config.bitDepth
         });
     }
-
-    stream(stream) {
-        if (this._stream) this._stream.unpipe(this._speaker);
-        this._stream = stream;
-        if (this._playing) this._stream.pipe(this._speaker);
+    
+    load(source) {
+        if (this._playing && this._audio) this._audio.unpipe(this._speaker);
+        this._audio = source ? new AudioStream(source) : null;
+        if (this._playing && this._audio) this._audio.pipe(this._speaker);
     }
 
     play() {
+        if (this._playing) throw 'Already playing';
+        if (this._audio) this._audio.pipe(this._speaker);
         this._playing = true;
-        if (this._stream) this._stream.pipe(this._speaker);
     }
 
     pause() {
+        if (!this.playing) throw 'Already paused';
+        if (this._audio) this._audio.unpipe(this._speaker);
         this._playing = false;
-        if (this._stream) this._stream.unpipe(this._speaker);
-    }
-
-    volume(volume) {
-        console.log('volume');
     }
 
     seek(seconds) {
-        if (this._stream) this._stream.seek(seconds);
+        if (!this._audio) throw 'No audio loaded';
+        this._audio.seek(seconds);
+    }
+    
+    volume(volume) {
+        console.log('volume');
     }
 
     next() {
@@ -46,6 +51,10 @@ class Player {
 
     shuffle(state) {
         console.log('shuffle', state);
+    }
+
+    get playing() {
+        return this._playing;
     }
 }
 
